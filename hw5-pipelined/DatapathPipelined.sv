1e17d81103a1507fc1b50a162248a7cd9a90b383
aychocho <aycho@seas.upenn.edu>
2025-03-20 20:24:33 -0400

    wip

--- hw5-pipelined/DatapathPipelined.sv
+++ hw5-pipelined/DatapathPipelined.sv
@@ -196,6 +196,7 @@ module DatapathPipelined (
   /***************/
 
   logic [`REG_SIZE] f_pc_current;
+  logic [`REG_SIZE] thisFetchInstruction;
   wire [`REG_SIZE] f_insn;
   cycle_status_e f_cycle_status;
 
@@ -260,28 +261,574 @@ module DatapathPipelined (
   // TODO: your code here, though you will also need to modify some of the code above
   // TODO: the testbench requires that your register file instance is named `rf`
 
-  //bypass
+  //wd bypass
+
+  logic[`REG_SIZE] thisRs1Data;
+  logic[`REG_SIZE] thisRs2Data;
+  logic[`REG_SIZE] thisDecodeInstruction;
+  logic[`REG_SIZE] nextDecodePc;
+  cycle_status_e thisDecodeCycleStatus;
+
   always_comb begin
     thisRs1Data = rs1_data;
     //forwarding
     if (regwe == 1'b1 && rd == rs1 && rs1 != 5'd0) begin
-      thisRs1Data = Writeback_data.result;
+      thisRs1Data = writebackData.result;
     end
     thisRs2Data = rs2_data;
     if (regwe == 1'b1 && rd == rs2 && rs2 != 5'd0) begin
-      thisRs2Data = Writeback_data.result;
+      thisRs2Data = writebackData.result;
     end
     //TODO: IF BRANCH
     
     //TODO: Essentially handle branches, and otherwise package up state in a packed struct
 
-  // EXECUTE[
+  // EXECUTE
+
+  //TODO: SET STAGE STATE PACKED
+  /*
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      set pc to 0
+      set instruction to 0
+      cycle status is reset
+    end else begin
+      pc is next
+      insn is current
+      cycle status is current
+      load rs1 and rs2
+    end
+  end  
+  */
+
+
+  //TODO: COPY PASTED FROM HW4, DEBUG LATER
+
+  //TODO: SET STATE
+
+  logic branchTime; //are we branching?
+  logic [`REG_SIZE] branchTo; //target addy
+
+  logic illegal_insn;
+  logic [`REG_SIZE] mem_addr;
+  logic [1:0] mem_off;
+  logic[63:0] mulhu_res, mulh_res;
+  logic[63:0] mulhsu_res;
+  logic N;
+  logic [`REG_SIZE] i_dividend_mag ;
+  logic [`REG_SIZE] i_divisor_mag ;
   
+
+  always_comb begin
+    illegal_insn = 1'b0;
+    //write enable bit, need to declare elsewhere
+    regwe = 1'b0;
+    //data reg, need to implement elsewhere
+    dataReg = 32'd0;
+    //default no branch
+    branchTime = 1'b0;
+    branchTo = 32'd0;
+    a = 0;
+	rsd1 = 64'b1;
+	rsd2 = 64'b1;
+    b = 0;
+    cin = 0;
+	//signed division stuffs 
+	N = 0;
+	i_dividend_mag = 32'd1;
+	i_divisor_mag = 32'd1;
+	
+	//divide state stuffs 
+	div_counter_next = 0;
+	while_divide_next = 0;
+	
+	
+	divisor = 32'b1;
+	dividend = 32'd2;
+	divisor_s = 32'b1;
+	dividend_s = 32'b1;
+	
+	
+    rd = insn_rd;
+    rs1 = insn_rs1;
+    rs2 = insn_rs2;
+    halt = 0;
+    pcNext = pcCurrent + 4;
+	addr_to_dmem = 4;
+	mem_off = 0;
+	mem_addr = 0;
+	store_we_to_dmem = 0;
+	store_data_to_dmem = 0;
+	mulhu_res = rs1_data*rs2_data;
+	mulh_res = $signed(rs1_data)*$signed(rs2_data);
+	mulhsu_res = $signed(rs1_data)*$signed({{1'b0},rs2_data});
+    case (insn_opcode)
+      OpLui: begin
+        // TODO: start here by implementing lui
+        // is there a 20 bit immediate? Not sure abt that, added it above for safety's sake
+        // imm_u should be u type immediate
+        //set write enable
+        regwe = 1'b1;
+        //send immu plus zeros to data
+        dataReg = {insn_from_imem[31:12], 12'b0};
+      end
+	  OpAuipc: begin
+		if(insn_auipc) begin
+			regwe = 1'b1;
+			dataReg = pcCurrent + (insn_from_imem[31:12]<<12) ;
+		end
+	  end
+	  OpJalr: begin
+		if(insn_jalr) begin
+			regwe = 1'b1;
+			dataReg = pcCurrent + 4;
+			pcNext = (rs1_data+imm_i_sext) & ~(32'h1);
+		end
+	  end
+      OpRegImm: begin
+        // TODO: do stuff that takes regs and an immediate, probably math log and shifts
+        regwe = 1'b1;
+        // case on fun3
+        if (insn_addi) begin
+          // logic for addi
+          a = rs1_data;
+          b = imm_i_sext;
+          cin = 1'b0;
+          dataReg = sum;
+        end
+
+        else if (insn_slti) begin
+          // logic for slti
+          dataReg = ($signed(rs1_data) < $signed(imm_i_sext)) ? 1:0;
+        end
+
+        else if (insn_sltiu) begin
+          // logic for sltiu
+          dataReg = ((rs1_data) < (imm_i_sext)) ? 1:0;
+        end
+
+        else if (insn_xori) begin
+          // logic for xori
+          dataReg = rs1_data ^ imm_i_sext;
+        end
+
+        else if (insn_ori) begin
+          // logic for ori
+          dataReg = rs1_data | imm_i_sext;
+        end
+
+        else if (insn_andi) begin
+          // logic for andi
+          dataReg = rs1_data & imm_i_sext;
+        end
+
+        else if (insn_slli) begin
+          // logic for slli
+          dataReg = rs1_data << imm_i[4:0];
+        end
+
+        else if (insn_srli) begin
+          dataReg = rs1_data >> imm_i[4:0];
+        end
+
+        else if (insn_srai) begin
+          // logic for srai
+          dataReg = $signed(rs1_data) >>> imm_i[4:0];
+        end
+        else //illegal case
+          begin
+            regwe = 1'b0;
+            illegal_insn = 1'b1;
+          end
+      end
+      OpRegReg: begin
+        // TODO: do stuff that takes all regs, same as above
+        //TODO: DONT FORGET ABT MULS!!! Need to add ifs
+        regwe = 1'b1;
+        // case on fun3
+        if (insn_add) begin
+          // add/sub case
+          // TODO: logic for add
+          a = rs1_data;
+          b = rs2_data;
+          cin = 1'b0;
+          dataReg = sum;
+        end
+        else if (insn_sub) begin
+          // TODO: logic for sub
+          a = rs1_data;
+          b = ~rs2_data;
+          cin = 1'b1;
+          dataReg = sum;
+        end
+        else if (insn_sll) begin
+          //TODO: THIS IS THE SAME FOR MULH
+          // logic for sll
+          dataReg = rs1_data << (rs2_data[4:0]);
+        end
+        else if (insn_slt) begin
+          // logic for slt
+          dataReg = $signed(rs1_data) < $signed(rs2_data) ? 32'd1:32'd0;
+        end
+        else if (insn_sltu) begin
+          // logic for sltu
+          dataReg = rs1_data < rs2_data ? 32'd1:32'd0;
+        end
+        else if (insn_xor) begin
+          // logic for xor
+          dataReg = rs1_data ^ rs2_data;
+        end
+        else if (insn_srl) begin
+          dataReg = rs1_data >> rs2_data[4:0];
+        end 
+        else if (insn_sra) begin
+          // logic for sra
+          dataReg = $signed(rs1_data) >>> rs2_data[4:0];
+        end
+        else if (insn_or) begin
+          // logic for or
+          dataReg = rs1_data | rs2_data;
+        end
+        else if (insn_and) begin
+          // logic for and
+          dataReg = rs1_data & rs2_data;
+        end
+		else if(insn_mul) begin
+		dataReg = rs1_data * rs2_data;
+		end
+		else if(insn_divu) begin
+			if(while_divide_current) begin
+				if(div_counter_current == 4'd7) begin
+					while_divide_next = 1'b0;
+					div_counter_next = 4'b0;
+					regwe = 1'b1;
+					dataReg = quotient_res;
+					pcNext = pcCurrent + 4;
+				end
+				else begin
+					regwe = 1'b0;
+					div_counter_next = div_counter_current + 4'b001;
+					while_divide_next = 1'b1;
+					pcNext = pcCurrent; 
+				end
+			end
+			else begin
+				while_divide_next = 1'b1;
+				div_counter_next = 4'b0001;
+				dividend = rs1_data;
+				divisor = rs2_data;
+				regwe = 1'b0;
+				pcNext = pcCurrent; 
+			end
+		end
+		else if(insn_remu) begin
+			if(while_divide_current) begin
+				if(div_counter_current == 4'd7) begin
+					while_divide_next = 1'b0;
+					div_counter_next = 4'b0;
+					regwe = 1'b1;
+					dataReg = remu_res;
+					pcNext = pcCurrent + 4;
+				end
+				else begin
+					regwe = 1'b0;
+					div_counter_next = div_counter_current + 4'b001;
+					while_divide_next = 1'b1;
+					pcNext = pcCurrent; 
+				end
+			end
+			else begin
+				while_divide_next = 1'b1;
+				div_counter_next = 4'b0001;
+				dividend = rs1_data;
+				divisor = rs2_data;
+				regwe = 1'b0;
+				pcNext = pcCurrent; 
+			end
+		end
+		else if(insn_rem) begin
+			if(while_divide_current) begin
+				if(div_counter_current == 4'd7) begin
+					while_divide_next = 1'b0;
+					div_counter_next = 4'b0;
+					regwe = 1'b1;
+					dataReg = (rs1_data[`REG_DIM-1]) ?( (~remu_res) + 32'b1): remu_res;
+					pcNext = pcCurrent + 4;
+				end
+				else begin
+					regwe = 1'b0;
+					div_counter_next = div_counter_current + 4'b001;
+					while_divide_next = 1'b1;
+					pcNext = pcCurrent; 
+				end
+			end
+			else begin
+				while_divide_next = 1'b1;
+				div_counter_next = 4'b0001;
+				i_dividend_mag = (rs1_data[`REG_DIM-1]==1'b1) ? (~rs1_data)+32'b1 : rs1_data;
+				i_divisor_mag = (rs2_data[`REG_DIM-1]==1'b1) ? (~rs2_data)+32'b1 : rs2_data;
+				dividend = i_dividend_mag;
+				divisor = i_divisor_mag;
+				regwe = 1'b0;
+				pcNext = pcCurrent; 
+			end
+		end
+		else if(insn_div) begin
+			if(while_divide_current) begin
+				if(div_counter_current == 4'd7) begin
+					N = rs1_data[`REG_DIM-1] ^ rs2_data[`REG_DIM-1];
+					while_divide_next = 1'b0;
+					div_counter_next = 4'b0;
+					regwe = 1'b1;
+					if(rs2_data==0) begin
+						dataReg = 32'hffff_ffff;
+					end
+					else begin
+						dataReg = (N)? ( (~quotient_res) + 32'b1) : quotient_res;
+					end
+					pcNext = pcCurrent + 4;
+				end
+				else begin
+					regwe = 1'b0;
+					div_counter_next = div_counter_current + 4'b001;
+					while_divide_next = 1'b1;
+					pcNext = pcCurrent; 
+				end
+			end
+			else begin
+				while_divide_next = 1'b1;
+				div_counter_next = 4'b0001;
+				i_dividend_mag = (rs1_data[`REG_DIM-1]==1'b1) ? (~rs1_data)+32'b1 : rs1_data;
+				i_divisor_mag = (rs2_data[`REG_DIM-1]==1'b1) ? (~rs2_data)+32'b1 : rs2_data;
+				dividend = i_dividend_mag;
+				divisor = i_divisor_mag;
+				regwe = 1'b0;
+				pcNext = pcCurrent; 
+			end
+		end
+		else if(insn_mulhu) begin
+			dataReg = mulhu_res[63:32];
+		end
+		else if(insn_mulh) begin
+			dataReg = mulh_res[63:32];
+		end
+		else if(insn_mulhsu) begin
+			dataReg = mulhsu_res[63:32];
+		end
+        else begin
+          regwe = 1'b0;
+          illegal_insn = 1'b1;
+        end
+      end
+	  OpLoad: begin
+		if(insn_lw) begin
+			if(((rs1_data + imm_i_sext)&(32'h0000_0003))==0) begin
+				regwe = 1'b1;
+				addr_to_dmem = rs1_data + imm_i_sext;
+				dataReg = load_data_from_dmem ; 
+			end
+			else begin
+				illegal_insn = 1'b1;
+			end
+		end
+		else if(insn_lb || insn_lbu) begin
+			regwe = 1'b1;
+			addr_to_dmem = (rs1_data + imm_i_sext)&(32'hffff_fffc);
+			mem_addr = rs1_data + imm_i_sext;
+			mem_off = mem_addr[1:0];
+			case (mem_off)
+				2'b00: dataReg = (insn_lb) ? {{24{load_data_from_dmem[7]}},load_data_from_dmem[7:0]} : {{24{1'b0}},load_data_from_dmem[7:0]};
+				2'b01: dataReg = (insn_lb) ?{{24{load_data_from_dmem[15]}},load_data_from_dmem[15:8]}: {{24{1'b0}},load_data_from_dmem[15:8]};
+				2'b10: dataReg = (insn_lb) ?{{24{load_data_from_dmem[23]}},load_data_from_dmem[23:16]}: {{24{1'b0}},load_data_from_dmem[23:16]};
+				2'b11: dataReg = (insn_lb) ?{{24{load_data_from_dmem[31]}},load_data_from_dmem[31:24]}: {{24{1'b0}},load_data_from_dmem[31:24]};
+			endcase 
+			
+		end
+		else if(insn_lh || insn_lhu) begin
+			regwe = 1'b1;
+			addr_to_dmem = (rs1_data + imm_i_sext)&(32'hffff_fffc);
+			mem_addr = rs1_data + imm_i_sext;
+			mem_off = mem_addr[1:0];
+			case (mem_off)
+				2'b00: dataReg = (insn_lh) ? {{16{load_data_from_dmem[15]}},load_data_from_dmem[15:0]} : {{16{1'b0}},load_data_from_dmem[15:0]};
+				2'b01: dataReg = (insn_lh) ? {{16{load_data_from_dmem[23]}},load_data_from_dmem[23:8]} : {{16{1'b0}},load_data_from_dmem[23:8]};
+				2'b10: dataReg = (insn_lh) ?{{16{load_data_from_dmem[31]}},load_data_from_dmem[31:16]}: {{16{1'b0}},load_data_from_dmem[31:16]};
+				2'b11: begin
+				regwe = 1'b0;
+				illegal_insn = 1'b1;
+				end
+			endcase 
+			
+		end
+		
+		else begin
+		  regwe = 1'b0;
+          illegal_insn = 1'b1;
+		end
+	  end
+	  OpStore: begin
+		if(insn_sw) begin
+			if(((rs1_data + imm_s_sext)&(32'h0000_0003))==0) begin
+				addr_to_dmem = rs1_data + imm_s_sext;
+				store_we_to_dmem = 4'hf;
+				store_data_to_dmem = rs2_data;
+			end
+			else begin
+				illegal_insn = 1'b1;
+			end
+		end
+		else if(insn_sh) begin
+			addr_to_dmem = (rs1_data + imm_s_sext)&(32'hffff_fffc);
+			mem_addr = rs1_data + imm_s_sext;
+			mem_off = mem_addr[1:0];
+			
+			case (mem_off)
+				2'b00: begin
+					store_we_to_dmem = 4'h3;
+					store_data_to_dmem = rs2_data;
+				end
+				2'b01: begin 
+					store_we_to_dmem = 4'h6;
+					store_data_to_dmem = {rs2_data[23:0],{8{1'b0}}};
+				end
+				2'b10: begin 
+					store_we_to_dmem = 4'hc;
+					store_data_to_dmem = {rs2_data[15:0],{16{1'b0}}};
+				end
+				2'b11: begin 
+					store_we_to_dmem = 4'h0;
+					store_data_to_dmem = 0;
+				end
+			endcase
+		end
+		else if(insn_sb) begin
+			addr_to_dmem = (rs1_data + imm_s_sext)&(32'hffff_fffc);
+			mem_addr = rs1_data + imm_s_sext;
+			mem_off = mem_addr[1:0];
+			
+			case (mem_off)
+				2'b00: begin
+					store_we_to_dmem = 4'h1;
+					store_data_to_dmem = rs2_data;
+				end
+				2'b01: begin 
+					store_we_to_dmem = 4'h2;
+					store_data_to_dmem = {rs2_data[23:0],{8{1'b0}}};
+				end
+				2'b10: begin 
+					store_we_to_dmem = 4'h4;
+					store_data_to_dmem = {rs2_data[15:0],{16{1'b0}}};
+				end
+				2'b11: begin 
+					store_we_to_dmem = 4'h8;
+					store_data_to_dmem = {rs2_data[7:0],{24{1'b0}}};
+				end
+			endcase
+				
+		end
+		else begin
+          illegal_insn = 1'b1;
+		end
+	  end
+      OpBranch: begin // Branch operations
+          if (insn_beq) begin
+            if (rs1_data == rs2_data) begin
+            branchTime = 1'b1; // Set branch condition true if registers are equal
+            branchTo = pcCurrent + imm_b_sext; // Calculate branch target
+            end
+          end else if (insn_bne) begin
+            if (rs1_data != rs2_data) begin
+            branchTime = 1'b1; // Set branch condition true if registers are not equal
+            branchTo = pcCurrent + imm_b_sext; // Calculate branch target
+            end
+          end else if (insn_blt) begin
+            if ($signed(rs1_data) < $signed(rs2_data)) begin
+            branchTime = 1'b1; // Set branch condition true if rs1 < rs2
+            branchTo = pcCurrent + imm_b_sext; // Calculate branch target
+            end
+          end else if (insn_bge) begin
+            if ($signed(rs1_data) >= $signed(rs2_data)) begin
+            branchTime = 1'b1; // Set branch condition true if rs1 >= rs2
+            branchTo = pcCurrent + imm_b_sext; // Calculate branch target
+            end
+          end else if (insn_bltu) begin
+            if (rs1_data < rs2_data) begin
+            branchTime = 1'b1; // Set branch condition true if rs1 < rs2 (unsigned)
+            branchTo = pcCurrent + imm_b_sext; // Calculate branch target
+            end
+          end else if (insn_bgeu) begin
+            if (rs1_data >= rs2_data) begin
+            branchTime = 1'b1; // Set branch condition true if rs1 >= rs2 (unsigned)
+            branchTo = pcCurrent + imm_b_sext; // Calculate branch target
+            end
+          end 
+      end
+	  OpJal : begin
+		regwe = 1'b1;
+		if(insn_jal) begin
+			dataReg = pcCurrent + 4;
+			pcNext = pcCurrent+ imm_j_sext;
+		end
+	  end
+      OpEnviron: begin  // ECALL and EBREAK
+          if (insn_ecall) begin
+            halt = 1'b1; 
+          end
+      end
+      default: begin
+        illegal_insn = 1'b1;
+      end
+    endcase
+    
+    if (branchTime) begin
+      pcNext = branchTo; // change pc to branch target
+    end
+  end
 
   // MEMORY
 
+  
+
   // WRITEBACK
 
+  stage_writeback_t writebackState;
+  data writebackData;
+
+  //update registers
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      writebackState.pc <= 0;
+      writebackState.insn <= 0;
+      writebackState.cycle_status <= CYCLE_RESET;
+      writebackData.we <= 0;
+      writebackData.result <= 0;
+      writebackData.rs1_data <= 0;
+      writebackData.rs2_data <= 0;
+    end else begin
+      begin
+          writebackState <= memory_state;
+          writebackData <= memory_data;
+      end
+    end
+  end
+
+  //writeback logic + halting
+  always_comb begin
+    regwe = writebackData.we;
+    dataReg = writebackData.result;
+    rd = writebackState.insn[11:7];
+    trace_writeback_cycle_status = writebackState.cycle_status;
+    trace_writeback_insn = writebackState.insn;
+    trace_writeback_pc = writeback_state.pc;
+
+    halt = 1'b0;
+    if(writebackState.insn == opcodeEnviron) begin
+      halt = 1'b1;
+    end
+  end
+
+
+  //TODO:disasm step
 
 endmodule
 
